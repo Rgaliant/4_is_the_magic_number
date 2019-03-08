93f168b01c7dfdf92fcc09a1bf242a6094c67c80
Ryan Gale <ryanwfgale@gmail.com>
2019-03-08 11:33:23 -0700

    init commit

---
+++ magicnumber.rb
@@ -0,0 +1,25 @@
+require 'humanize'
+
+
+
+puts "enter a number"
+num = gets.to_i
+
+def magicNumber(num)
+  num1 = num.humanize
+  num1arr = num1.split('')
+  num2 = num1arr.length
+  if num2 == num
+    puts "#{num} is the magic number"
+    exit
+  else 
+    puts "#{num} is #{num2}"
+    magicNumber(num2)
+  end
+end
+magicNumber(num)
+
+
+
+
+
