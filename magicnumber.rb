c756d9ce0a7746f0e9d57d6ebb759f837a26858d
Ryan Gale <ryanwfgale@gmail.com>
2019-03-08 11:38:01 -0700

    add colorize

--- magicnumber.rb
+++ magicnumber.rb
@@ -1,4 +1,5 @@
 require 'humanize'
+require 'colorize'
 
 
 
@@ -10,13 +11,14 @@ def magicNumber(num)
   num1arr = num1.split('')
   num2 = num1arr.length
   if num2 == num
-    puts "#{num} is the magic number"
+    puts "#{num} is the magic number".colorize(:green)
     exit
   else 
     puts "#{num} is #{num2}"
     magicNumber(num2)
   end
 end
+
 magicNumber(num)
 
 
